5610e8c403524ce254b19cc1bfdd7e836327fc30
NzauM <mercywaenu16@gmail.com>
2020-01-11 14:56:13 +0300

    Add the profile class and a method to get all projects

--- Ratings/models.py
+++ Ratings/models.py
@@ -1,4 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete = models.CASCADE)
+    profile_pic = models.ImageField(upload_to='pictures/')
+    bio = models.TextField(max_length=1000)
+    info = models.TextField(max_length=5000)
+    
+class Projects(models.Model):
+    '''
+    Class for instantiating all projects objects
+    '''
+    title = models.CharField(max_length=500)
+    description=models.TextField(max_length=2000)
+    link=models.URLField()
+    image=models.ImageField(upload_to='pictures/')
+
+    def save_project(self):
+        '''
+        Function for saving a project
+        '''
+        self.save()
+
+    @classmethod
+    def all_projects(cls):
+        '''
+        Function for getting all projects
+        '''
+        all_projects = cls.objects.all()
+        return all_projects
