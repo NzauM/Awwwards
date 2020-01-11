f5a5c01bd14a456449a31766a659fc6d0b4800ec
NzauM <mercywaenu16@gmail.com>
2020-01-11 15:25:12 +0300

    Install pillow to handle image upload

--- Ratings/views.py
+++ Ratings/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Profile,Projects
 
 # Create your views here.
+# @login_required(login_url = '/accounts/login/')
+def home(request):
+    '''
+    View function to render thr home page
+    '''
+    all_projects = Projects.all_projects()
+    return render(request,'home.html',{'all_projets':all_projects})
+
 
